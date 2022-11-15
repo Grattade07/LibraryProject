d8181e94d7fb87a29aaa7f0889a5f2496a49b2ad
Grattade07 <joe.woodcock15@hotmail.co.uk>
2022-11-15 18:02:24 +0000

    Removed old code

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-/*function Book(title , author, pages , read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
-    
-    this.info = function() {
-      return title + " by " + author + ", " + pages + " pages " + ", " + read }
-  } 
-  
-  const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet")
-  
-  theHobbit.info()
-  
-  console.log(theHobbit.info()) */
-
 //Variables
 const book = document.createElement("div");
 const title = document.createElement("div")
